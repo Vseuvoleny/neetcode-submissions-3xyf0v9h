c04e7f5be5b119fb395c1f3c9038318d8bc36248
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-04-15 00:33:32 +0400

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let result = '';
+        let counter = 0;
+
+        while (true){
+            const subresult = strs[0][counter]
+            if (subresult === undefined){
+                return result
+            } 
+            const isEveryEqual =  strs.every((s)=>s[counter]===subresult)
+            if (isEveryEqual){
+                result+=subresult
+            } else {
+                return result
+            }
+            counter++
+        }
+    }
+}
+
