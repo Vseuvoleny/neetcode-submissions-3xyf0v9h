650062343b2dca9f9c31947a593ec0eba9bfa7ed
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-04-14 14:08:38 +0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+        if (s.length !==t.length){
+            return false
+        }
+
+        let count = new Array(26).fill(0);
+        for (let i =0; i < s.length; i++){
+            
+            count[s.charCodeAt(i) - 'a'.charCodeAt()]++
+            count[t.charCodeAt(i) - 'a'.charCodeAt()]--
+        
+        }
+        return count.filter(Boolean).length === 0
+    }
+
+    }
