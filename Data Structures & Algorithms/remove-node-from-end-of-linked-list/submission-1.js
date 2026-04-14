34ee92a36b804cfa8cec998874420fd9d04f7891
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-04-14 16:28:09 +0400

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.js
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    removeNthFromEnd(head, n) {
+        let current = head;
+        let prev = null;
+        let targetPrev = null;  // Узел ПЕРЕД удаляемым
+        let counter = 1;
+        
+        while (current) {
+            // // Когда прошли n узлов, начинаем двигать targetPrev
+            // if (counter === n) {
+            //     targetPrev = head;
+            // } else if (counter > n) {
+            //     targetPrev = targetPrev.next;
+            // }
+            
+            // Сохраняем предыдущий узел для удаления
+            if (counter === n + 1) {
+                prev = head;
+            } else if (counter > n + 1) {
+                prev = prev.next;
+            }
+            
+            counter++;
+            current = current.next;
+        }
+        
+        // Если targetPrev === null, значит удаляем голову
+        if (!prev) {
+            return head.next;
+        }
+
+        // Удаляем узел
+        prev.next = prev.next.next;
+        
+        return head;
+    }
+}
+
+
+// [1,2,3,4],2
+// 1 итерация
+//  isDeleted = 1;
+//  prev = null;
+//  counter = 1;
+//  current = 1;
+// 2 итерация
+//  isDeleted = 1;
+//  prev = 1;
+//  counter = 2;
+//  current = 2;
+// 3 итерация
+//  current = 3;
+//  isDeleted = 2;
+//  prev = 2;
+//  counter = 3;
+// 4 итерация
+//  isDeleted = 2;
+//  prev = 3;
+//  counter = 4;
