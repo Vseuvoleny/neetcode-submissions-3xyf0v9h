37da338284ff1939dd8d780264651349984b324b
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-04-15 16:20:42 +0400

    Add: valid-palindrome-ii - submission-13

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-13.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+       isPalindrome(s) {
+        let left = 0,
+            right = s.length - 1;
+        while (left < right) {
+            if (s[left] !== s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    validPalindrome(s) {
+        let right = s.length-1;
+        let left = 0;
+        while (left<right){
+            const leftElem = s[left]
+            const rightElem = s[right]
+      
+            if (leftElem !==rightElem){
+               return this.isPalindrome(s.slice(0, left) + s.slice(left + 1)) ||
+                    this.isPalindrome(s.slice(0, right) + s.slice(right + 1))
+            }
+            left++
+            right--
+        }
+
+        return true
+    }
+}
