e909e9e741c7a69508c115dc46be0dfe95576a41
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-04-18 00:16:35 +0400

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        const stack = [];
+        let result = 0
+        for (const op of operations){
+            switch (op){
+                case '+':
+                const res = Number(stack.at(-1))+ Number(stack.at(-2))
+                stack.push(res)
+                break
+                case 'C':
+                stack.pop();
+                break
+                case "D":
+                const ress = Number(stack.at(-1)) * 2;
+                stack.push(ress)
+                break
+                default:
+                stack.push(op)
+            }
+        }
+
+        for (const st of stack){
+            result +=Number(st) 
+        }
+        return result
+    }
+}
