3a8dd01d8aece2691be5f17897c774eb0479d94b
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-04-21 00:02:22 +0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let stack = [];
+        const resultArr = [];
+        for (let i = 0;i<temperatures.length;i++){
+            const elem = temperatures[i];
+            let days = i
+
+            while (days<temperatures.length){
+                stack.push(temperatures[days])
+                if (stack.at(-1) > elem){
+                    resultArr.push(stack.length-1);
+                    stack =[]
+                    break
+                }  else if (days===temperatures.length-1){
+                         resultArr.push(0);
+                         stack=[]
+                         break
+                } else {
+                    days++
+                }
+             
+            }
+    }
+    return resultArr
+}
+}
