a8b6aaca9106a1868e8e8ff2a34c1adf30eeaa1b
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-04-21 23:04:25 +0400

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+
+
+
+    majorityElement(nums) {
+        let counter = 0 ;
+        const map = {};
+        let result = 0;
+        while(counter<nums.length){
+            const elem = nums[counter];
+            if (map[elem] === undefined){
+                map[elem] = 1 
+            } else {
+                   map[elem] = map[elem]+1 
+            }
+            counter++
+        }
+        counter = 0
+        console.log({map})
+        return Object.entries(map).reduce((acc,[key,value])=>{
+                console.log({value})
+            if (value > counter){
+                counter=value
+                acc=key
+            }
+
+            return acc
+        },result)
+    }
+}
