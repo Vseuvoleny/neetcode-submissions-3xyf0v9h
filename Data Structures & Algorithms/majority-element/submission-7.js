83819129e64a3de002bf3ae0e79e4062a6812f7d
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-04-21 23:08:45 +0400

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+
+
+
+    majorityElement(nums) {
+        let counter = 0 ;
+        const map = {};
+        let result = 0;
+        let max = 0;
+        while(counter<nums.length){
+            const elem = nums[counter];
+            map[elem] = (map[elem] || 0)+1
+            if (map[elem] > max){
+                result = elem
+                max = map[elem]
+            }
+            counter++
+        }
+        return result
+    }
+}
