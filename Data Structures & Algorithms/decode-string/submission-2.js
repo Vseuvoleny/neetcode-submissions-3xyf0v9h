7edc7d9d3034de410d23e49d1e08c47237758945
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-04-28 00:10:22 +0400

    Add: decode-string - submission-2

---
+++ Data Structures & Algorithms/decode-string/submission-2.js
@@ -0,0 +1,36 @@
+class Solution {
+
+
+
+
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    decodeString(s) {
+        const stack = [];
+        for (let elem of s){
+            if (elem !== ']') {
+                stack.push(elem)
+            } else {
+                    let subString = '';
+                    let digit = '';
+   
+                while (stack.length>0 && stack[stack.length - 1] !=='['){
+                    subString = stack.pop() + subString;
+                    continue
+                }
+                stack.pop();
+                while (stack.length>0 && !isNaN(stack[stack.length - 1])){
+                    digit = stack.pop() + digit
+                    continue
+                }
+
+                const repeated = subString.repeat(Number(digit));
+                stack.push(repeated)
+            }
+
+        }
+          return stack.join('')
+    }
+}
