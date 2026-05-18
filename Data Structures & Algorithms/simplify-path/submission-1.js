35dd58864430b047a98da31e5807ead68f443e10
Arthur <51128713+Vseuvoleny@users.noreply.github.com>
2026-05-19 01:44:43 +0400

    Add: simplify-path - submission-1

---
+++ Data Structures & Algorithms/simplify-path/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} path
+     * @return {string}
+     */
+    simplifyPath(path) {
+        const stack = [];
+        const splitted = path.split('/')
+        splitted.forEach((s)=>{
+            if (s) {
+            if (s === '..'){
+                stack.pop()
+            }  else if (s!== '.'){
+                stack.push(s)
+            }
+            }
+        })
+        console.log({splitted,stack})
+
+        return `/${stack.join('/')}`
+    }
+}
